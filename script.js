b54bd45b1034e8dfbbd7d648aeae20d7d234ec00
AdetunjiJakes <68664742+Jamleads@users.noreply.github.com>
2023-06-22 00:11:30 -0400

    modal on click, convert screen,

--- script.js
+++ script.js
@@ -49,3 +49,43 @@ if (document.querySelector('.airtime')) {
     });
   }
 }
+
+if (document.querySelector('.swap-convert')) {
+  document.querySelector('.swap-prev').addEventListener('click', () => {
+    document.querySelector('.blur-section').classList.add('blur');
+    document.querySelector('.swap-modal').classList.remove('hidden');
+  });
+
+  document.querySelector('.swap-cancel').addEventListener('click', () => {
+    document.querySelector('.blur-section').classList.remove('blur');
+    document.querySelector('.swap-modal').classList.add('hidden');
+  });
+
+  document.querySelector('.swap-submit').addEventListener('click', () => {
+    window.location.href = './tracasction-success.html?source=convert';
+  });
+}
+
+if (document.querySelector('.transaction-sucessfull')) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const source = urlParams.get('source');
+  console.log(source);
+
+  if (source === 'convert') {
+    document.querySelector('.note-head').textContent = 'Swap successful';
+
+    const paragraphs = document.getElementsByTagName('p');
+    paragraphs[0].textContent = '';
+    paragraphs[1].textContent =
+      'You have successfully swapped 100 USD to 74,000 NGN ';
+
+    document.querySelector('.swap-done').addEventListener('click', () => {
+      console.log('go back to home');
+      window.location.href = './home.html';
+    });
+
+    document.querySelector('.view-receipt').addEventListener('click', () => {
+      console.log('show receipt');
+    });
+  }
+}
